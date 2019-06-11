366501ea7cb7a3fe8803c87bdc4f4a5bf5d66c91
J A <jarata@gmail.com>
2019-06-11 12:58:03 -0700

    more while examples

--- while_example.py
+++ while_example.py
@@ -2,3 +2,20 @@ spam = 0
 while spam < 5:
 	print('Hello world!')
 	spam = spam + 1
+
+print('\n')
+
+name = ''
+while name != 'your name':
+	print('Please type your name.')
+	name = input()
+print('Thank you!')
+
+print('\n')
+
+spam = 0
+while spam < 5:
+	spam = spam + 1
+	if spam == 3:
+		continue
+	print('spam is ' + str(spam))
