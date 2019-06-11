e775786cc50e99dd4e3f54074195df028112685f
J A <jarata@gmail.com>
2019-06-11 13:08:33 -0700

    loop examples

---
+++ loop_example.py
@@ -0,0 +1,24 @@
+print('My name is')
+for i in range(5):
+	print('Jimmy Five Times ' + str(i))
+
+print('\n')
+
+total = 0
+for num in range(101):
+	total = total + num
+print(total)
+
+print('\n')
+
+print('My name is ')
+i = 0
+while i < 5:
+	print('Jimmy Five Times ' + str(i))
+	i = i + 1
+
+print('\n')
+
+print('My name is ')
+for i in range(12, 16):
+	print('Jimmy Five Times ' + str(i))
